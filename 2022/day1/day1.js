388c089a721c49af0c53985e97ea8a09800fee6d
Daniele Ferrari CP <daniele.ferrari.work@gmail.com>
2023-09-08 18:05:26 +0200

    part 2 resolved

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -7,27 +7,24 @@ export default function day1() {
 
   let elfsCalories = [0];
   let currentElfIndex = 0;
-  let fatterElfIndex = 0;
 
   input.split("\n").forEach((element) => {
-
     if (element == "\r") {
-      
-      let currentElfCalories = elfsCalories[currentElfIndex];
-
-      if (elfsCalories[fatterElfIndex] <= currentElfCalories)
-        fatterElfIndex = currentElfIndex;
-
       currentElfIndex = currentElfIndex + 1;
       elfsCalories[currentElfIndex] = 0;
-
     } else {
-
       let calories = Math.floor(element);
       elfsCalories[currentElfIndex] = elfsCalories[currentElfIndex] + calories;
-    
     }
   });
 
-  console.log(fatterElfIndex, " : ", elfsCalories[fatterElfIndex])
+  let sortedElfsCalories = elfsCalories.sort().reverse();
+
+  // PART 1
+  // console.log(sortedElfsCalories[0]);
+
+  // PART 2
+  console.log(
+    sortedElfsCalories[0] + sortedElfsCalories[1] + sortedElfsCalories[2]
+  );
 }
