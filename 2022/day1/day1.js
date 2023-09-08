97e8501d6d1bd8a5cdb5d1ff6eaf62fed96a054c
Daniele Ferrari CP <daniele.ferrari.work@gmail.com>
2023-09-08 17:55:50 +0200

    part 1 resolved

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -3,4 +3,31 @@ import fs from "fs";
 export default function day1() {
   console.log("DAY1");
 
+  const input = fs.readFileSync("./day1/day1-input", "utf8");
+
+  let elfsCalories = [0];
+  let currentElfIndex = 0;
+  let fatterElfIndex = 0;
+
+  input.split("\n").forEach((element) => {
+
+    if (element == "\r") {
+      
+      let currentElfCalories = elfsCalories[currentElfIndex];
+
+      if (elfsCalories[fatterElfIndex] <= currentElfCalories)
+        fatterElfIndex = currentElfIndex;
+
+      currentElfIndex = currentElfIndex + 1;
+      elfsCalories[currentElfIndex] = 0;
+
+    } else {
+
+      let calories = Math.floor(element);
+      elfsCalories[currentElfIndex] = elfsCalories[currentElfIndex] + calories;
+    
+    }
+  });
+
+  console.log(fatterElfIndex, " : ", elfsCalories[fatterElfIndex])
 }
