79be76848738f5906a45df6053b24c32b9759842
Daniele Ferrari <daniele.ferrari.work@gmail.com>
2023-12-07 00:13:56 +0100

    prepared 2023 day1

---
+++ 2023/day1/day1.js
@@ -0,0 +1,12 @@
+import fs from "fs";
+
+export default function day1() {
+  console.log("--- DAY1 ---");
+
+  const input = fs.readFileSync("./day1/day1-input", "utf8");
+
+  console.log("Part 1: ");
+  
+  console.log("Part 2: ");
+
+}
